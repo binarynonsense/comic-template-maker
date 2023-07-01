0441eddc0681d3abc11a78166af47662d009f6d6
Álvaro García <alvaro@binarynonsense.com>
2023-07-01 16:05:31 +0200

    Reorganize init code

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const g_version = "1.0.0";
 init();
 
 function init() {
-  document.getElementById("info-version-p").innerHTML = `${g_version}`;
+  initBase();
   initCanvas();
   initPresets();
   initSaveLoad();
@@ -31,73 +31,81 @@ export function getVersion() {
   return g_version;
 }
 
-document
-  .getElementById("refresh-button")
-  .addEventListener("click", function () {
-    drawTemplate();
-  });
+function initBase() {
+  document.getElementById("info-version-p").innerHTML = `${g_version}`;
 
-let refreshable = document.querySelectorAll(".refresh");
-for (let i = 0; i < refreshable.length; i++) {
-  refreshable[i].addEventListener("change", function (event) {
-    if (event.target.id === "preset-select") {
-      if (event.target.value != 0) {
-        setPreset(-1, true); // load all defaults
-        setPreset(event.target.value - 1, true);
-        if (document.getElementById("autorefresh-checkbox").checked) {
-          drawTemplate();
-          resetView();
-        }
-        event.target.value = 0;
-      }
-    } else if (event.target.id === "grid-preset-select") {
-      if (event.target.value != 0) {
-        setGridPreset(event.target.value - 1);
-        if (document.getElementById("autorefresh-checkbox").checked) {
-          drawTemplate();
-          resetView();
-        }
-        event.target.value = 0;
-      }
-    } else {
-      if (event.target.id === "layout-template-select") {
-        if (
-          document.getElementById("layout-template-select").value === "page"
-        ) {
-          document.getElementById("layout-page-div").classList.remove("hidden");
-          document
-            .getElementById("layout-thumbnails-div")
-            .classList.add("hidden");
-        } else {
-          document.getElementById("layout-page-div").classList.add("hidden");
-          document
-            .getElementById("layout-thumbnails-div")
-            .classList.remove("hidden");
-        }
-      }
-      if (document.getElementById("autorefresh-checkbox").checked)
-        drawTemplate();
-    }
-  });
-}
+  document
+    .getElementById("refresh-button")
+    .addEventListener("click", function () {
+      drawTemplate();
+    });
 
-for (let i = 1; i < 8; i++) {
-  const tab = document.getElementById(`tab-${i}`);
-  tab.addEventListener("click", function () {
-    if (!tab.classList.contains("tab-selected")) {
-      for (let j = 1; j < 8; j++) {
-        if (i === j) {
-          document.getElementById(`tab-${j}`).classList.add("tab-selected");
-          document
-            .getElementById(`tab-${j}-content`)
-            .classList.remove("hidden");
-        } else {
-          document.getElementById(`tab-${j}`).classList.remove("tab-selected");
-          document.getElementById(`tab-${j}-content`).classList.add("hidden");
+  let refreshable = document.querySelectorAll(".refresh");
+  for (let i = 0; i < refreshable.length; i++) {
+    refreshable[i].addEventListener("change", function (event) {
+      if (event.target.id === "preset-select") {
+        if (event.target.value != 0) {
+          setPreset(-1, true); // load all defaults
+          setPreset(event.target.value - 1, true);
+          if (document.getElementById("autorefresh-checkbox").checked) {
+            drawTemplate();
+            resetView();
+          }
+          event.target.value = 0;
+        }
+      } else if (event.target.id === "grid-preset-select") {
+        if (event.target.value != 0) {
+          setGridPreset(event.target.value - 1);
+          if (document.getElementById("autorefresh-checkbox").checked) {
+            drawTemplate();
+            resetView();
+          }
+          event.target.value = 0;
+        }
+      } else {
+        if (event.target.id === "layout-template-select") {
+          if (
+            document.getElementById("layout-template-select").value === "page"
+          ) {
+            document
+              .getElementById("layout-page-div")
+              .classList.remove("hidden");
+            document
+              .getElementById("layout-thumbnails-div")
+              .classList.add("hidden");
+          } else {
+            document.getElementById("layout-page-div").classList.add("hidden");
+            document
+              .getElementById("layout-thumbnails-div")
+              .classList.remove("hidden");
+          }
+        }
+        if (document.getElementById("autorefresh-checkbox").checked)
+          drawTemplate();
+      }
+    });
+  }
+
+  for (let i = 1; i < 8; i++) {
+    const tab = document.getElementById(`tab-${i}`);
+    tab.addEventListener("click", function () {
+      if (!tab.classList.contains("tab-selected")) {
+        for (let j = 1; j < 8; j++) {
+          if (i === j) {
+            document.getElementById(`tab-${j}`).classList.add("tab-selected");
+            document
+              .getElementById(`tab-${j}-content`)
+              .classList.remove("hidden");
+          } else {
+            document
+              .getElementById(`tab-${j}`)
+              .classList.remove("tab-selected");
+            document.getElementById(`tab-${j}-content`).classList.add("hidden");
+          }
         }
       }
-    }
-  });
+    });
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
